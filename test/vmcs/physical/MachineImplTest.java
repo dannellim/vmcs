538b275d1cd9dd73d2c589cde73f20423c3e6252
HtetAung <htetaung.ycc.pku@gmail.com>
2020-11-29 17:07:00 +0800

    added test

---
+++ test/vmcs/physical/MachineImplTest.java
@@ -0,0 +1,53 @@
+package vmcs.physical;
+
+
+public class MachineImplTest {
+
+    @org.junit.Test
+    public void initStocks() {
+    }
+
+    @org.junit.Test
+    public void dispenseDrink() {
+    }
+
+    @org.junit.Test
+    public void updateDrinkStock() {
+    }
+
+    @org.junit.Test
+    public void updateDrinkPrice() {
+    }
+
+    @org.junit.Test
+    public void updateCoinStock() {
+    }
+
+    @org.junit.Test
+    public void acceptCoin() {
+    }
+
+    @org.junit.Test
+    public void getAllDrinks() {
+    }
+
+    @org.junit.Test
+    public void getAllCoins() {
+    }
+
+    @org.junit.Test
+    public void dispense() {
+    }
+
+    @org.junit.Test
+    public void unlockDoor() {
+    }
+
+    @org.junit.Test
+    public void lockDoor() {
+    }
+
+    @org.junit.Test
+    public void testDispense() {
+    }
+}
